22ab9ca8a5aa7e0f277a82a0ff54412dbcd2b1c4
Arjun Anand <arju.n.anand@outlook.com>
2025-07-16 20:35:54 +0530

    Initial commit: Add madlibs project

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "arjun "
+#
+# print(youtuber  + "is a great youtuber")
+# print("{}is a great youtuber".format(youtuber))
+# print(f"{youtuber}is a great youtuber")
+
+adj = input('Enter your Adjective: ')
+verb = input('Enter your Verb: ')
+famous_person = input('Enter your Famous Person: ')
+print(f'Hi i am a computer science programmer and I am {adj}. I like to stay hydrated because it lets me {verb} with concentration. \
+When I\'m {verb}ing I feel like {famous_person}')
+
+
+
